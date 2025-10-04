a01aab61d9f421103364f2e79d19b110107f71b9
oddodoo <mabyannugroho225@gmail.com>
2025-10-04 07:54:47 +0700

    Add initial HTML, CSS, and JavaScript files for Todo List application

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let todos = [];
+
+function addTodo() {
+    const todoInput = document.getElementById('todo-input');
+    const todoDate = todoInput.value.trim();
+
+     /// Check if input is not empty
+    if (todoInput) {}
+}
