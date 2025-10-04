db63a74873df5fa9c79f3f42f66344efa4ea9f3e
oddodoo <mabyannugroho225@gmail.com>
2025-10-04 11:41:20 +0700

    Refactor CSS styles and update HTML structure for Todo List application

--- js/script.js
+++ js/script.js
@@ -1,9 +1,1 @@
-let todos = [];
-
-function addTodo() {
-    const todoInput = document.getElementById('todo-input');
-    const todoDate = todoInput.value.trim();
-
-     /// Check if input is not empty
-    if (todoInput) {}
-}
+console.log("Hello, World!");
